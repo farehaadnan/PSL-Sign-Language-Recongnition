ede5626fac1c7102d4fc92bd4bcc47a4e904d96d
farehaadnan <farehaadnan493@gmail.com>
2026-05-08 15:10:59 +0500

    Update app.py

--- app.py
+++ app.py
@@ -58,9 +58,9 @@ mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 hands_detector = mp_hands.Hands(
     static_image_mode=False,
-    max_num_hands=2,
-    min_detection_confidence=0.7,
-    min_tracking_confidence=0.7
+    max_num_hands=1,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5
 )
 # ── State ──────────────────────────────────────────────────────────────────────
 state = {
